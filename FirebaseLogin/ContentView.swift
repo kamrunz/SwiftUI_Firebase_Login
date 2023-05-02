9a02f419360771619c9064a8484538261c5ce96f
kamrunz <kamraan.namazov@gmail.com>
2023-05-02 15:28:35 +0400

    Initial Commit

---
+++ FirebaseLogin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirebaseLogin
+//
+//  Created by Kamran Namazov on 02.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseLogin/FirebaseLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseLoginApp.swift
+//  FirebaseLogin
+//
+//  Created by Kamran Namazov on 02.05.23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
